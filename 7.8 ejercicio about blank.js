7af1a4abb0f1f12acbd1c2369f93d787b2ff2a5b
JhonIsNotReal <110693901+Jhonisnotreal@users.noreply.github.com>
2022-11-21 15:30:56 -0600

    Add files via upload

---
+++ 7.8 ejercicio about blank.js
@@ -0,0 +1,10 @@
+//7.8 Ejercicio en about:blank
+
+// - Abre una nueva ventana "about:blank" en Google Chrome
+
+// - Abre la consola de desarrollador de Google Chrome (F12)
+
+// - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+// const edad = Number(prompt("¿Cúal es tu edad?"))
+//Ahora en la consola llama a edad --> edad
+//Ya esta listo, ahora tienes la edad del usuario
